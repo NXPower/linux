5919e15a51fed52b6123953554faf148ff36470b
Malcolm Crossley <malcolm@nutanix.com>
2016-09-14 15:20:31 -0700

    KVM: x86: Update vCPU CPUID after resetting vCPU CR4 registers
    Summary:
    OSXSAVE and OSPKE CPUID bits are dependent on CR4 enable bits.
    To match real hardware we need to reset the CPUID bits when we reset the
    CR4 bits otherwise OSXSAVE and OSPKE may be erroneously enabled.
    Test Plan: dev test
    Breaks Rolling Upgrade: no
    Target Release: Asterix
    Tickets Resolved: ENG-61546
    Reviewers: cui, felipe.franciosi, mukesh.rathor, gasmith
    Reviewed By: gasmith
    CC: ahv
    Differential Revision: http://codereview.nutanix.net/D11796

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -8594,6 +8594,7 @@ void kvm_vcpu_reset(struct kvm_vcpu *vcpu, bool init_event)
 	vcpu->arch.ia32_xss = 0;
 
 	kvm_x86_ops->vcpu_reset(vcpu, init_event);
+	kvm_update_cpuid(vcpu);
 }
 
 void kvm_vcpu_deliver_sipi_vector(struct kvm_vcpu *vcpu, u8 vector)
